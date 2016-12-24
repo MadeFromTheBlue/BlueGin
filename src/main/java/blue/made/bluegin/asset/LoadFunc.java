4af1aacdce130c1cfd000d807543215bbb808d8e
Sam Sartor <me@samsartor.com>
2016-12-23 20:30:35 -0700

    Move to angle project for now

--- src/main/java/blue/made/bluegin/asset/LoadFunc.java
+++
@@ -1,9 +0,0 @@
-package blue.made.bluegin.asset;
-
-/**
- * Created by Sam Sartor on 12/23/2016.
- */
-@FunctionalInterface
-public interface LoadFunc<A, S extends AssetSource<A>> {
-    public A load(S source, String group, String id) throws Exception;
-}
